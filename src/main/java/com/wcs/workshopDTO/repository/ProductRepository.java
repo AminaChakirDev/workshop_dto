cec6c1bb5f3ea5675b343c5919448763af611400
AminaChakirDev <amina.aitm@gmail.com>
2023-06-02 11:29:59 +0200

    Init without dto

---
+++ src/main/java/com/wcs/workshopDTO/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.wcs.workshopDTO.repository;
+
+import com.wcs.workshopDTO.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findProductsByCategory_Id(Long categoryId);
+}
---
+++ src/test/java/com/wcs/workshopDTO/WorkshopDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.workshopDTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
